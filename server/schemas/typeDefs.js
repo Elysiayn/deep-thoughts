b95ab0b8481073b8cf5546274ef6e02da6941672
Elysiayn <wendy.lemus.wl@gmail.com>
2021-03-26 14:55:49 -0400

    updated w/ codesnap

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,24 +1,6 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
-  type Thought {
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-  }  
-
-  type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
-  }
-
   type User {
     _id: ID
     username: String
@@ -26,6 +8,22 @@ const typeDefs = gql`
     friendCount: Int
     thoughts: [Thought]
     friends: [User]
+  }
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
   }
 
   type Query {
@@ -36,6 +34,4 @@ const typeDefs = gql`
   }
 `;
 
-
-// export the typeDefs
 module.exports = typeDefs;
